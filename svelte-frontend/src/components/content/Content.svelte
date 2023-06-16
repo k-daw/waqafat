0443b759bc4d9386375a8538def5b211ac35a0d1
k-daw <daw@aucegypt.edu>
2023-06-17 01:11:00 +0200

    wip, displaying selected text

--- svelte-frontend/src/components/content/Content.svelte
+++ svelte-frontend/src/components/content/Content.svelte
@@ -2,17 +2,35 @@
     import Verse from "./Verse.svelte";
     import quran from "../../../quran.json";
 	import VirtualList from '@sveltejs/svelte-virtual-list';
+    import SelectedWords from "./SelectedWords.svelte";
 
     let items: (string | number)[][] = quran;
     let start: any;
     let end: any;
+
+    let sw: string[] = [];
    
    const setSelectedWords = (verseNumber: number, index: number) => {
-        console.log("index:", index, "verse number: ",verseNumber);
+        const selection: string = `${verseNumber},${index}`;
+    
+        if (sw.includes(selection)) {
+            let removingIndex = sw.indexOf(selection);
+            sw.splice(removingIndex, 1);
+        }
+        else {
+            sw.push(selection);
+        }
+    
+        sw = sw.sort();
+
+        console.log("selected words from content:", sw);
    }
 
 </script>
 
+<div>
+    <SelectedWords selectedWords={sw} />
+</div>
 <div class="rtl-grid container">
     <VirtualList {items} bind:start bind:end let:item>
         <Verse verseText={item[1]} verseNumber={item[3]} setSelection={setSelectedWords}/>
---
+++ svelte-frontend/src/components/content/SelectedWords.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+    export let selectedWords: string[];
+    import quran from "../../../quran.json";
+
+</script>
+
+<div class="flex justify-center align-center w-3/4 h" >
+    <div class="bg-grey-300">
+        {#each selectedWords  as t}
+            <span>{quran[parseInt(t.split(',')[0])-1][1].split(' ')[[parseInt(t.split(',')[1])]]}</span>
+            <span>  </span>
+        {/each}
+    </div>
+</div>
